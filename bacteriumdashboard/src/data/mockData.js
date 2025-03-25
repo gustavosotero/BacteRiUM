633e9a9ab89d6a946778e70d030c857f02c10926
darkhallow123 <daviddogs120@gmail.com>
2025-03-25 10:12:15 -0400

    implementing manageTeam Page (Almost Done)

--- bacteriumdashboard/src/data/mockData.js
+++ bacteriumdashboard/src/data/mockData.js
@@ -2,10 +2,29 @@ import { tokens } from "../theme";
 
 export const mockDataTeam = [
   {
-    fullName: "",
-    role: "",
-    email: "",
-  }
+    id: 1,
+    fullName: "Francisco",
+    role: "admin",
+    email: "francisco@upr.edu"
+  },
+  {
+    id: 2,
+    fullName: "Gustavo",
+    role: "admin",
+    email: "gustavo@upr.edu"
+  },
+  {
+    id: 3,
+    fullName: "Edgar",
+    role: "user",
+    email: "edgar@upr.edu"
+  },
+  {
+    id: 4,
+    fullName: "Anton",
+    role: "user",
+    email: "anton@upr.edu"
+  },
 ];
 
 export const mockEnvironmentData = [
--- bacteriumdashboard/src/scenes/manageTeam/ManageTeam.jsx
+++ bacteriumdashboard/src/scenes/manageTeam/ManageTeam.jsx
@@ -1,23 +1,94 @@
-import {Box, Typography, useTheme} from "@mui/material";
+import {Box, Button, Typography, useTheme} from "@mui/material";
 import Headers from "../../Components/Headers";
 import {tokens} from "../../theme";
 import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
 import PersonIcon from '@mui/icons-material/Person';
-//import {mockData} from '../../Components/Assets/data/mockData'; for testing use
+import { mockDataTeam } from "../../data/mockData";
 import {DataGrid} from "@mui/x-data-grid";
 
 
 const ManageTeam = () => {
 
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const columns = [
+        {
+            field: "id", headerName:"ID",
+        },
+        {
+            field: "fullName", 
+            headerName: "FullName",
+            flex: 1,
+            cellClassName: "name-column--cell",
+        },
+        {
+            field: "email",
+            headerName: "Email",
+            flex: 1,
+        },
+        {
+            field: "role",
+            headerName: "Role",
+            flex: 1,
+            // renderCell: ({row: {access}}) => {
+            //     return (<Box>
+            //         <Button>
+
+            //         </Button>
+            //     </Box>);
+            // }
+        },
+        {
+            field: "action",
+            headerName: "Action",
+            flex: 1,
+            renderCell: ({row: {action}}) => {
+                return (<Box>
+                    <Button color="secondary">
+                        Remove User
+                    </Button>
+                </Box>);
+            }
+        }
+    ];
+
     return (
-       <Box margin={"20px"}>
-
-
-        <Headers title={"Manage Team"}/>
-
-
+        <Box margin={"20px"}>
+            <Headers title={"Manage Team"}/>
+            <Box m="40px 0 0 0"
+            height={"75vh"}
+            sx={
+                {
+                    "& .MuiDataGrid-root": {
+                        border: "none",
+                    },
+                    "& .MuiDataGrid-cell": {
+                        borderBottom: "none",
+                    },
+                    "& .name-column--cell": {
+                        color: colors.greenAccent[300],
+                    },
+                    "& .MuiDataGrid-columnHeaders": {
+                        backgroundColor: colors.blueAccent[700],
+                        borderBottom: "none",
+                    },
+                    "& .MuiDataGrid-virtualScroller": {
+                        backgroundColor: colors.primary[400],
+                    },
+                    "& .MuiDataGrid-footerContainer": {
+                        borderTop: "none",
+                        backgroundColor: colors.blueAccent[700],
+                    },
+                    "& .MuiCheckbox-root": {
+                        color: `${colors.greenAccent[200]} !important`,
+                    },
+                }
+            }
+            >
+                <DataGrid checkboxSelection rows={mockDataTeam} columns={columns}/>
+            </Box>
        </Box>
-    )
+    );
 }
 
 export default ManageTeam;
